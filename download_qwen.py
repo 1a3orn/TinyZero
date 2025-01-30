4370f3a92cd64e7c3aff4ae9bcf100faa3bb0ac9
1a3orn <1a3orn@protonmail.com>
2025-01-30 11:15:57 -0500

    ASD

--- download_qwen.py
+++ download_qwen.py
@@ -1,8 +1,8 @@
 import transformers
 
-model_name = "Qwen/Qwen2-5B"
+model_name = "Qwen/Qwen2.5-1.5B"
 
 
-output_path = "./models/Qwen2-5B"  # Change this to your desired path
+output_path = "./models/Qwen2.5-1.5B"  # Change this to your desired path
 model = transformers.AutoModelForCausalLM.from_pretrained(model_name)
 model.save_pretrained(output_path, use_safetensors=True)
